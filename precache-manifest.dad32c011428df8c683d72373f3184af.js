caec4e1b3396ab696c24a3af906cb58a75aa7a30
Kseniya Antipina <karinpark@mail.ru>
2020-10-23 16:10:50 +0300

    Updates

---
+++ precache-manifest.dad32c011428df8c683d72373f3184af.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434a527196d1ef9feb9a041fb5d6113e",
+    "url": "/cat-food/index.html"
+  },
+  {
+    "revision": "3db76d9e948eff54b4e6",
+    "url": "/cat-food/static/css/main.16284782.chunk.css"
+  },
+  {
+    "revision": "70d5e047b723ba3a28d7",
+    "url": "/cat-food/static/js/2.0f1964a1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cat-food/static/js/2.0f1964a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db76d9e948eff54b4e6",
+    "url": "/cat-food/static/js/main.c3d35777.chunk.js"
+  },
+  {
+    "revision": "04d12276cb23ac32ceb1",
+    "url": "/cat-food/static/js/runtime-main.c3a21e33.js"
+  },
+  {
+    "revision": "07bcbc412552463425af87c47efec87b",
+    "url": "/cat-food/static/media/Cat.07bcbc41.png"
+  },
+  {
+    "revision": "8e412cf5e5954e6134e979e7f9cb9e17",
+    "url": "/cat-food/static/media/Pattern.8e412cf5.png"
+  },
+  {
+    "revision": "d479314e3c9aa85ccbd24ef14841d9d0",
+    "url": "/cat-food/static/media/Pattern@2x.d479314e.png"
+  },
+  {
+    "revision": "9b45a5e6fd7e43502d74c1cf2bb77ded",
+    "url": "/cat-food/static/media/TrebuchetMS-Bold.9b45a5e6.woff"
+  },
+  {
+    "revision": "34723a2fbef5882977e8392d020956af",
+    "url": "/cat-food/static/media/TrebuchetMS.34723a2f.woff"
+  },
+  {
+    "revision": "69e1c8424d1953bc76a67c60269a9a3f",
+    "url": "/cat-food/static/media/exo2thin.69e1c842.woff2"
+  }
+]);
